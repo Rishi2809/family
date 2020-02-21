29d1daadbc535cd4dee50ab884316b72926d0698
Rishi2809 <Rishiparakh28@gmail.com>
2020-02-21 21:42:28 +0530

    acitivity files

---
+++ app/src/main/java/com/rishi/family/GiftRecActivity.java
@@ -0,0 +1,14 @@
+package com.rishi.family;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class GiftRecActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_gift_rec);
+    }
+}
